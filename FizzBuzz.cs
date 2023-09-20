3634543559076b958dd86136c2b79aae81601286
StruggsJ <strjac5@gmail.com>
2023-09-20 16:17:15 -0500

    Added FizzBuzz.cs File

---
+++ FizzBuzz.cs
@@ -0,0 +1,20 @@
+
+public string FizzBuzz(number input)
+{
+    if (input % 3 = 0 && input % 5 == 0)
+    {
+        return "fizzbuzz";
+    }
+    else if (input % 5)
+    {
+        return "buzz";
+    }
+    else if (input % 3 = 0)
+    {
+        return "fizz";
+    }
+    else
+    {
+        return "";
+    }
+}
